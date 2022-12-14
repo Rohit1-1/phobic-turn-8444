070e292f7b04365f8aebdc4497090a1f759be0b2
salonidhatrak <salonidhatrak123@gmail.com>
2022-12-14 22:53:42 +0530

    day1 task

--- 911.com/src/App.js
+++ 911.com/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Ayurveda from './Pages/Aurvedas';
+
 
 function App() {
   return (
     <div className="App">
-    
+    <Ayurveda/>
     </div>
   );
 }
---
+++ 911.com/src/Pages/Aurvedas.jsx
@@ -0,0 +1,11 @@
+import HealthCondition from "../components/HealthCondition";
+import HealthCancern from "../components/HeathConcern";
+
+export default function Ayurveda (){
+    return(
+        <>
+        <HealthCondition/>
+        <HealthCancern/>
+        </>
+    )
+}
